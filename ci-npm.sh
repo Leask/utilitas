169b09ec401ad9a621e40236a6a12e32adcc769e
Leask Wong <i@leaskh.com>
2020-06-02 23:57:40 -0400

    update npm script

--- ci-npm.sh
+++ ci-npm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-npm version patch && \
-git commit -am 'npm version patch' && \
-npm publish && \
-git pull && \
-git push
+npm version patch \
+&& git commit -am 'npm version patch' \
+&& npm publish \
+&& git pull \
+&& git push
