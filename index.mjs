f4e9a2d32c5491622abc6d96f47171d19199045f
Leask Wong <i@leaskh.com>
2022-03-19 02:13:27 -0400

    working on browser version building

--- index.mjs
+++ index.mjs
@@ -1,39 +1,132 @@
 // dependencies
-export { default as colors } from 'colors/safe.js';
-export { default as fetch } from 'node-fetch';
-export { Tail as tail } from 'tail';
-export * as base64url from 'base64url';
-export * as fileType from 'file-type';
-export * as geoIp from 'fast-geoip';
-export * as ini from 'ini';
-export * as jwt from 'jsonwebtoken';
-export * as mailgun from 'mailgun-js';
-export * as mailjet from 'node-mailjet';
-export * as math from 'mathjs';
-export * as mysql from 'mysql2/promise';
-export * as ping from 'ping';
-export * as qs from 'qs';
-export * as redis from 'ioredis';
-export * as sentry from '@sentry/node';
-export * as telegraf from 'telegraf';
-export * as telesign from 'telesignsdk';
-export * as twilio from 'twilio';
-export * as uuid from 'uuid';
-
+import { default as colors } from 'colors/safe.js';
+import { default as fetch } from 'node-fetch';
+import { Tail as tail } from 'tail';
+import * as base64url from 'base64url';
+import * as fileType from 'file-type';
+import * as geoIp from 'fast-geoip';
+import * as ini from 'ini';
+import * as jwt from 'jsonwebtoken';
+import * as mailgun from 'mailgun-js';
+import * as mailjet from 'node-mailjet';
+import * as math from 'mathjs';
+import * as mysql from 'mysql2/promise';
+import * as ping from 'ping';
+import * as qs from 'qs';
+import * as redis from 'ioredis';
+import * as sentry from '@sentry/node';
+import * as telegraf from 'telegraf';
+import * as telesign from 'telesignsdk';
+import * as twilio from 'twilio';
+import * as uuid from 'uuid';
 // features
-export * as bot from './lib/bot.mjs';
-export * as cache from './lib/cache.mjs';
-export * as dbio from './lib/dbio.mjs';
-export * as email from './lib/email.mjs';
-export * as encryption from './lib/encryption.mjs';
-export * as event from './lib/event.mjs';
-export * as network from './lib/network.mjs';
-export * as sentinel from './lib/sentinel.mjs';
-export * as shell from './lib/shell.mjs';
-export * as shot from './lib/shot.mjs';
-export * as sms from './lib/sms.mjs';
-export * as storage from './lib/storage.mjs';
-export * as tape from './lib/tape.mjs';
-export * as uoid from './lib/uoid.mjs';
-export * as utilitas from './lib/utilitas.mjs';
+import * as bot from './lib/bot.mjs';
+import * as cache from './lib/cache.mjs';
+import * as dbio from './lib/dbio.mjs';
+import * as email from './lib/email.mjs';
+import * as encryption from './lib/encryption.mjs';
+import * as event from './lib/event.mjs';
+import * as network from './lib/network.mjs';
+import * as sentinel from './lib/sentinel.mjs';
+import * as shell from './lib/shell.mjs';
+import * as shot from './lib/shot.mjs';
+import * as sms from './lib/sms.mjs';
+import * as storage from './lib/storage.mjs';
+import * as tape from './lib/tape.mjs';
+import * as uoid from './lib/uoid.mjs';
+import * as utilitas from './lib/utilitas.mjs';
+
+// Export
 export * as default from './lib/utilitas.mjs';
+export {
+    // dependencies
+    colors,
+    fetch,
+    tail,
+    base64url,
+    fileType,
+    geoIp,
+    ini,
+    jwt,
+    mailgun,
+    mailjet,
+    math,
+    mysql,
+    ping,
+    qs,
+    redis,
+    sentry,
+    telegraf,
+    telesign,
+    twilio,
+    uuid,
+    // features
+    bot,
+    cache,
+    dbio,
+    email,
+    encryption,
+    event,
+    network,
+    sentinel,
+    shell,
+    shot,
+    sms,
+    storage,
+    tape,
+    uoid,
+    utilitas,
+};
+
+// Browser
+let runningInBrowser = false;
+try { runningInBrowser = !!window; } catch (e) { }
+// import * as package from './package.json';
+// https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const pack = {
+    ...await storage.readJson('./package.json'),
+    runningInBrowser,
+};
+if (runningInBrowser) {
+    window._utilitas = pack;
+    window.utilitas = {
+        // dependencies
+        colors,
+        fetch,
+        tail,
+        base64url,
+        fileType,
+        geoIp,
+        ini,
+        jwt,
+        mailgun,
+        mailjet,
+        math,
+        mysql,
+        ping,
+        qs,
+        redis,
+        sentry,
+        telegraf,
+        telesign,
+        twilio,
+        uuid,
+        // features
+        bot,
+        cache,
+        dbio,
+        email,
+        encryption,
+        event,
+        network,
+        sentinel,
+        shell,
+        shot,
+        sms,
+        storage,
+        tape,
+        uoid,
+        utilitas,
+    };
+    console.log('[UTILITAS](https://github.com/Leask/utilitas) is ready!');
+} else { global._utilitas = pack; }
