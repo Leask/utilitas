b01d8cadebdb03d1cbbceab92aae88109bb16ab3
Leask Wong <i@leaskh.com>
2022-03-22 13:57:55 -0400

    upgraded to node-fetch v3

---
+++ build.mjs
@@ -0,0 +1,10 @@
+import { writeFile } from './lib/storage.mjs';
+import manifest from './package.json' assert { type: 'json' };
+
+// https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const strManifest = [
+    `const manifest = ${JSON.stringify(manifest, null, 4)};`,
+    'export default manifest;',
+].join('\n\n');
+
+await writeFile('./lib/manifest.mjs', strManifest);
