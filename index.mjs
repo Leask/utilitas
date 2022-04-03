4246601468a0472cd552b51c033cb08845a9be6e
Leask Wong <i@leaskh.com>
2022-04-03 18:44:12 -0400

    tweak import

--- index.mjs
+++ index.mjs
@@ -1,8 +1,4 @@
 // dependencies
-import { default as formData } from 'form-data';
-import { default as mailgun } from 'mailgun.js';
-import { default as mailjet } from 'node-mailjet';
-import { default as qrcode } from 'qrcode';
 import { Tail as tail } from 'tail';
 import { Telegraf as telegraf } from 'telegraf';
 import * as base64url from 'base64url';
@@ -18,9 +14,12 @@ import * as sentry from '@sentry/node';
 import * as telesign from 'telesignsdk';
 import * as twilio from 'twilio';
 import * as uuid from 'uuid';
+import formData from 'form-data';
+import mailgun from 'mailgun.js';
+import mailjet from 'node-mailjet';
+import qrcode from 'qrcode';
 // features
 import _ from './lib/horizon.mjs'
-import { default as color } from './lib/color.mjs';
 import * as bot from './lib/bot.mjs';
 import * as cache from './lib/cache.mjs';
 import * as dbio from './lib/dbio.mjs';
@@ -36,6 +35,7 @@ import * as storage from './lib/storage.mjs';
 import * as tape from './lib/tape.mjs';
 import * as uoid from './lib/uoid.mjs';
 import * as utilitas from './lib/utilitas.mjs';
+import color from './lib/color.mjs';
 import manifest from './lib/manifest.mjs';
 
 // Export
