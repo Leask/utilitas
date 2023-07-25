499d4c75a3e0450f98794c44e369606d6408ea2b
Leask Wong <i@leaskh.com>
2023-07-24 23:45:17 -0400

    update dependencies

--- index.mjs
+++ index.mjs
@@ -45,8 +45,8 @@ export {
 
 if (utilitas.inBrowser() && !globalThis.utilitas) {
     globalThis.utilitas = {
-        boxes, color, encryption, event, manifest, math, shekel, shot, storage,
-        uoid, utilitas, uuid,
+        boxes, color, encryption, event, manifest, math, shekel, shot, speech,
+        storage, uoid, utilitas, uuid,
     };
     // top-level await workaround
     (async () => {
