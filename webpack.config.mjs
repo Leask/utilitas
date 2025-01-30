c0cdd7b350ed832ebfb2df0a8c72114e25463852
Leask Wong <i@leaskh.com>
2025-01-30 00:29:23 -0500

    update dependencies

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -83,7 +83,7 @@ export default {
     ignoreWarnings: [warning =>
         (warning?.loc?.start?.line === 75 && warning?.loc?.start?.column === 31 && warning?.loc?.end?.line === 75 && warning?.loc?.end?.column === 57)   // event
         ||
-        (warning?.loc?.start?.line === 749 && warning?.loc?.start?.column === 44 && warning?.loc?.end?.line === 749 && warning?.loc?.end?.column === 56) // utilitas
+        (warning?.loc?.start?.line === 752 && warning?.loc?.start?.column === 44 && warning?.loc?.end?.line === 752 && warning?.loc?.end?.column === 56) // utilitas
     ],
     // stats: 'detailed',
 };
