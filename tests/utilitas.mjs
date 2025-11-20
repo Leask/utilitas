4e0ad33028034baf834dad48c84177deb78fa920
Leask Wong <i@leaskh.com>
2025-11-20 02:52:52 -0500

    Add new file for utility functions in tests
    Created a new file named utilitas.mjs containing utility functions to be used in test cases. This file will help streamline testing processes and improve code organization.

---
+++ tests/utilitas.mjs
@@ -0,0 +1,35 @@
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+import { utilitas } from '../index.mjs';
+
+test('utilitas ensureInt', () => {
+    const int = utilitas.ensureInt(1, { pad: 2 });
+    assert.equal(int, '01');
+});
+
+test('utilitas range', () => {
+    const range = utilitas.range(12, 3, { noBoundary: true });
+    assert.deepEqual(range, [11, 10, 9, 8, 7, 6, 5, 4]);
+});
+
+test('utilitas extract', () => {
+    const obj = { a: { b: { x: 111 } } };
+    const val = utilitas.extract(obj, 'a', 'b', 'x');
+    assert.equal(val, 111);
+    
+    const valNull = utilitas.extract(null, 'a', 'b', 'x');
+    assert.equal(valNull, null);
+});
+
+test('utilitas ensureString', () => {
+    const str = utilitas.ensureString(null);
+    assert.equal(str, '');
+});
+
+test('utilitas hexEncode/Decode', () => {
+    const str = 'asdfasdf11111';
+    const hex = utilitas.hexEncode(str);
+    assert.equal(hex, '61736466617364663131313131');
+    const decoded = utilitas.hexDecode(hex);
+    assert.equal(decoded, str);
+});
