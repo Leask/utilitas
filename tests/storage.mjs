2c237e4f0656eccda796af31d33bf34f38525d06
Leask Wong <i@leaskh.com>
2025-12-04 02:42:06 -0500

    Update storage test file to improve coverage and fix failing test case.

--- tests/storage.mjs
+++ tests/storage.mjs
@@ -19,3 +19,13 @@ test('storage writeTempFile', async () => {
     const readBack = await storage.readJson(tempPath);
     assert.deepEqual(readBack, data);
 });
+
+test('storage config', async () => {
+    const filename = await storage.getConfigFilename();
+    assert.ok(filename, 'Config filename should be present');
+    const config = { test: 'ok' };
+    const setConfig = await storage.setConfig(config);
+    assert.deepEqual(setConfig.config.test, config.test, 'Config should be set');
+    const getConfig = await storage.getConfig();
+    assert.deepEqual(getConfig.config.test, config.test, 'Config should be retrieved');
+});
