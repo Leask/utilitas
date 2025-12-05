e64589ed6220cf5a3cb9ed6e42503272c14b77ee
Leask Wong <i@leaskh.com>
2025-12-05 01:08:07 -0500

    Update speech test to include additional test case for better coverage.

--- tests/speech.mjs
+++ tests/speech.mjs
@@ -15,16 +15,22 @@ const GOOGLE_KEY = config?.google_key;
 const skipReasonOpenAI = !OPENAI_KEY && 'openai_key is missing from config.json';
 const skipReasonGoogle = !GOOGLE_KEY && 'google_key is missing from config.json';
 
-test('speech tts openai', { skip: skipReasonOpenAI, timeout: 1000 * 60 }, async () => {
+test('speech tts/stt openai', { skip: skipReasonOpenAI, timeout: 1000 * 60 }, async () => {
     await speech.init({ provider: 'OpenAI', tts: true, stt: true, apiKey: OPENAI_KEY });
     const audio = await speech.tts('testing, this is a test.', { expected: 'BUFFER' });
     assert.ok(Buffer.isBuffer(audio), 'Audio should be a buffer');
     assert.ok(audio.length > 0, 'Audio should not be empty');
+    const text = await speech.stt(audio, { input: 'BUFFER' });
+    assert.ok(typeof text === 'string', 'Text should be a string');
+    assert.ok(text.length > 0, 'Text should not be empty');
 });
 
-test('speech tts google', { skip: skipReasonGoogle, timeout: 1000 * 60 * 5 }, async () => {
-    await speech.init({ provider: 'Google', tts: true, apiKey: GOOGLE_KEY });
+test('speech tts/stt google', { skip: skipReasonGoogle, timeout: 1000 * 60 * 5 }, async () => {
+    await speech.init({ provider: 'Google', tts: true, stt: true, apiKey: GOOGLE_KEY });
     const audio = await speech.tts('testing, this is a test.', { expected: 'BUFFER' });
     assert.ok(Buffer.isBuffer(audio), 'Audio should be a buffer');
     assert.ok(audio.length > 0, 'Audio should not be empty');
+    const text = await speech.stt(audio, { input: 'BUFFER' });
+    assert.ok(typeof text === 'string', 'Text should be a string');
+    assert.ok(text.length > 0, 'Text should not be empty');
 });
