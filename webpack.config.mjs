2bfef5066cc70821129df3cd18d408e6b9ef3a3e
Leask Wong <i@leaskh.com>
2025-12-14 04:30:02 -0500

    Updated webpack configuration to include new plugins and optimizations for better performance and build output.

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -50,6 +50,7 @@ export default {
             'os': false,
             'worker_threads': false,
             'file-type': 'file-type/core',
+            [utilitas.__(import.meta.url, 'lib/callosum.mjs')]: false,
         },
     },
     externals: [
@@ -86,7 +87,7 @@ export default {
     ignoreWarnings: [warning =>
         (warning?.loc?.start?.line === 75 && warning?.loc?.start?.column === 31 && warning?.loc?.end?.line === 75 && warning?.loc?.end?.column === 57)   // event
         ||
-        (warning?.loc?.start?.line === 766 && warning?.loc?.start?.column === 44 && warning?.loc?.end?.line === 766 && warning?.loc?.end?.column === 56) // utilitas
+        (warning?.loc?.start?.line === 782 && warning?.loc?.start?.column === 44 && warning?.loc?.end?.line === 782 && warning?.loc?.end?.column === 56) // utilitas
     ],
     // stats: 'detailed',
 };
